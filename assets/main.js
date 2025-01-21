865758e45d9cd00f3dac1e1a5ef4295a20b03b3c
Carlos Colorado <caancoqu@gmail.com>
2025-01-20 21:49:59 -0500

    Se hizo el llamado a la API de youtube y funciona correctamente

--- assets/main.js
+++ assets/main.js
@@ -14,6 +14,7 @@ const options = {
 async function fetchData(urlApi) {
     const response = await fetch(urlApi, options);
 	const data = await response.json();
+    return data;
 }
 
 
@@ -25,7 +26,7 @@ async function fetchData(urlApi) {
              <div class="group relative">
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
           </div>
           <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
@@ -34,7 +35,7 @@ async function fetchData(urlApi) {
             </h3>
           </div>
         </div>`).slice(0,4).join('')}
-       `;
+       `; content.innerHTML = view;
     } catch (error) {
         console.error(error);
     }
